23f1a986f516a00129cfdd4998a712888602e5cd
kolcompass <kolcompass@proton.me>
2025-08-03 00:55:10 +1000

    Fix Python package structure and imports

--- leverage/juliaos/leverage/__init__.py
+++ leverage/juliaos/leverage/__init__.py
@@ -6,6 +6,11 @@ from ..leverage_integration_helper import *
 from ..universal_leverage_system_core import *
 from ..leverage_induction import *
 
+# Re-export main classes
+from ..leverage_integration_helper import LeverageIntegration
+from ..universal_leverage_system_core import LeverageSystem
+from ..leverage_induction import LeverageInduction
+
 __all__ = [
     'LeverageSystem',
     'LeverageInduction',
